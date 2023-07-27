6cb10af69940c8baea47b2472e5ec753b8a8ef6b
SUKRON-KATSIR <Sukron.katsir98@gmail.com>
2023-07-27 11:24:51 +0700

    siukm_pmk

---
+++ index1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<link rel="stylesheet" type="text/css" href="style1.css">
+  <meta charset="UTF-8">
+  <title>Tampilan Awal</title>
+</head>
+
+<body>
+<div id="container">
+<header>
+  <div id="logo" >
+  </div>
+  <section>
+    <h1><hs> APLIKASI SIUKM</hs>
+    <!-- <t id="tanggal"><?php echo date("d M Y"); ?></t> -->
+  </h1>
+    <hx >Sistem Informasi <span>Usaha Kecil Menengah</span></hx>
+    <hx>Kabupaten Pamekasan</hx>
+  </section>
+</header>
+
+
+  <article>
+<!--  -->
+  </article>
+
+<aside>
+  <section>
+    <h1>Pilihan Menu</h1>
+  </section>
+  <menu>
+    <li><a href="index1.php">Home</li>
+    <li><a href="tambah_anggota.php">Input Data</a></li>
+    <li><a href="edit_anggota.php">Update Data</a></li>
+    <li><a href="tampil_anggota.php">Tampilkan Data</a></li>
+    <li><a href="hapus_anggota.php">Hapus Data</a></li>
+    <li><a href="about.php">Tentang Kami</a></li>
+    <li><a href="logout.php">Logout</a></li>
+  </menu>
+  
+</aside>
+
+<footer>
+  <div>
+  Copyright © <?php echo date("Y"); ?> Mahasiswa Universitas Madura| Designed by: Sukron Katsir
+  </div>
+</footer>
+
+</div>
+</body>
+</html>
